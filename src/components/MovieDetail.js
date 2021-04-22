9782fa988515416d2418cc67688b2581dfb715a8
Denis <dineresc@gmail.com>
2021-04-22 15:24:33 -0300

    add stars for watching movies

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,6 +1,19 @@
 import { Card, CardContent } from "@material-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  star_full: {
+    color: "orange",
+  },
+  star_empty: {
+    color: "gray",
+  },
+});
 
 const MovieDetail = (props) => {
+  const classes = useStyles();
   return (
     <div>
       {props.movie ? (
@@ -8,6 +21,46 @@ const MovieDetail = (props) => {
           <CardContent>
             <h2>{props.movie?.title}</h2>
             <p>{props.movie?.description}</p>
+            <FontAwesomeIcon
+              icon={faStar}
+              className={
+                props.movie.avg_ratings > 0
+                  ? classes.star_full
+                  : classes.star_empty
+              }
+            />
+            <FontAwesomeIcon
+              icon={faStar}
+              className={
+                props.movie.avg_ratings > 1
+                  ? classes.star_full
+                  : classes.star_empty
+              }
+            />
+            <FontAwesomeIcon
+              icon={faStar}
+              className={
+                props.movie.avg_ratings > 2
+                  ? classes.star_full
+                  : classes.star_empty
+              }
+            />
+            <FontAwesomeIcon
+              icon={faStar}
+              className={
+                props.movie.avg_ratings > 3
+                  ? classes.star_full
+                  : classes.star_empty
+              }
+            />
+            <FontAwesomeIcon
+              icon={faStar}
+              className={
+                props.movie.avg_ratings > 4
+                  ? classes.star_full
+                  : classes.star_empty
+              }
+            />
           </CardContent>
         </Card>
       ) : null}
